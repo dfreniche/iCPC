5ccb724df315334ffabbb1e1528fe3c8c2c74970
Diego Freniche Brito <diego@freniche.com>
2019-02-21 20:21:18 +0100

    Moved instance variables to private interface

--- iCPC/Classes/CrocoDSViewController.m
+++ iCPC/Classes/CrocoDSViewController.m
@@ -27,6 +27,33 @@
     
 #define RAYON 20
 
+@interface crocodsViewController () {
+
+OGLView *m_oglView;
+CADisplayLink* m_displayLink;
+
+CGPoint centerLocation;
+
+UIView                  *inputView;
+
+MyKeyboard *mykeyboard;
+
+CGRect portraitBounds;
+
+UIImageView *key_up;
+UIImageView *key_down;
+UIImageView *key_left;
+UIImageView *key_right;
+UIImageView *key_a;
+UIImageView *key_b;
+UIImageView *key_select;
+UIImageView *key_start;
+
+GCController *myController;
+}
+
+@end
+
 @implementation crocodsViewController
 
 
