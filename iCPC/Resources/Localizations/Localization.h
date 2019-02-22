3deb8f3335a1bc76b399efabdf3a8c3c615c7eb0
Diego Freniche Brito <diego@freniche.com>
2019-02-22 12:08:31 +0100

    Run colloc, added localization files to project. From now on, we can run colloc and that´s all

---
+++ iCPC/Resources/Localizations/Localization.h
@@ -0,0 +1,5 @@
+/**	This is an automatically generated file	Please don't modify it.	If you need to change some text please do so at	https://docs.google.com/spreadsheets/d/1pAHay9k3s_X1wo-3XmC9NJjxpY2-Pk1NZrEdrpmpCgM/export?format=tsv&id=1pAHay9k3s_X1wo-3XmC9NJjxpY2-Pk1NZrEdrpmpCgM	*/#define ls_generic_yes NSLocalizedString(@"ls_generic_yes", nil)
+#define ls_generic_no NSLocalizedString(@"ls_generic_no", nil)
+#define ls_generic_ok NSLocalizedString(@"ls_generic_ok", nil)
+#define ls_generic_cancel NSLocalizedString(@"ls_generic_cancel", nil)
+#define ls_about NSLocalizedString(@"ls_about", nil)
