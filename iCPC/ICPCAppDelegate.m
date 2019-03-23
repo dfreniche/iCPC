30c3ca9e18e3054ea0fdfb2dc23bac65438576bf
Diego Freniche Brito <diego@freniche.com>
2019-03-23 13:48:12 +0100

    Cleaned AppDelegate: deleting mothods not used

--- iCPC/ICPCAppDelegate.m
+++ iCPC/ICPCAppDelegate.m
@@ -11,45 +11,12 @@
 
 @implementation ICPCAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:nil];
     
     [ICPCFileManager copyAllDSKFilesToDocumentDirectory];
     
     return YES;
-}
-
-
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-
 }
 
 - (BOOL)application:(UIApplication*)application openURL:(NSURL*)url sourceApplication:(NSString*)sourceApplication annotation:(id)annotation {
