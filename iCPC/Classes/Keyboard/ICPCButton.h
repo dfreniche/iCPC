a0784ae159e481fc1d6b8a1a70d350731623bf79
Diego Freniche Brito <diego@freniche.com>
2019-03-24 20:55:47 +0100

    New child VC with keyboard suppport

---
+++ iCPC/Classes/Keyboard/ICPCButton.h
@@ -0,0 +1,16 @@
+//
+//  ICPCButton.h
+//  iCPC
+//
+//  Created by Diego Freniche Brito on 24/03/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ICPCButton : UIButton
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iCPC/Classes/Keyboard/ICPCButton.m
@@ -0,0 +1,20 @@
+//
+//  ICPCButton.m
+//  iCPC
+//
+//  Created by Diego Freniche Brito on 24/03/2019.
+//
+
+#import "ICPCButton.h"
+
+@implementation ICPCButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
